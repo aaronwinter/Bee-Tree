469fcc93315775139ca5e1c574f4864715da6e7a
Aaron <aaron.ounn@gmail.com>
2016-10-18 18:42:46 -0400

    Create basic helpers and static methods

--- btree/bnode.py
+++ btree/bnode.py
@@ -8,6 +8,53 @@ class Node(object):
         self.count = 0
         self.isLeaf = True
 
+    @staticmethod
+    def _split_keys(arr):
+        lHalf = arr[:len(arr)/2]
+        rHalf = arr[len(arr)/2+1:]
+        median = arr[len(arr)/2]
+        return (lHalf, median, rHalf)
+
+    @staticmethod
+    def _split_pointers(arr):
+        return (arr[:len(arr)/2], arr[len(arr)/2:])
+    
+    def _is_full(self):
+       return len(self.keys) == 2 * self.order - 1
+
+    def _is_half_full(self):
+       return len(self.keys) == (2 * self.order - 1)/2
+    
+    def _is_overflowing(self):
+        return len(self.keys) >= self.order
+
+    def _is_underflowing(self):
+        return len(self.keys) <= (self.order/2 - 1)
+
+    def _get_index(self, k):
+        index = 0
+        for index, v in enumerate(self.keys):
+            if k < v: break
+
+        if index == (len(self.keys) - 1) and k > self.keys[-1]:
+            return index + 1
+        else:
+            return index
+    
+    def _get_index_pointer(self, target):
+        index = 0
+        for index, ptr in enumerate(self.pointers):
+            if target == ptr:
+                return index
+        return -1
+    
+    def _replace_key(self, k, new_k):
+        arr = self.keys
+        for i, v in enumerate(arr):
+            if v == k:
+                arr[i] = new_k
+                break
+        return arr
     def _search(self, key):
         if key in self.keys:
             return True
